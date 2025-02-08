f09f9da2eae7fde87724aa4d861b692423bd616d
chiaki-tachikawa <46268222+chiaki-tachikawa@users.noreply.github.com>
2025-02-08 15:10:11 +0000

    Created using Colab

---
+++ rectangle.py
@@ -0,0 +1,66 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOSJNExRmzwzkS8AzuVknLU",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/chiaki-tachikawa/hachi-test-repo/blob/test/rectangle.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "height = 10\n",
+        "width = 5"
+      ],
+      "metadata": {
+        "id": "gDOWRh2JFcJV"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def area(height, width):\n",
+        "  return height *width"
+      ],
+      "metadata": {
+        "id": "Zd-ioWK8Fr_F"
+      },
+      "execution_count": 2,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def edge_length( height, width):\n",
+        "  return 2*(height, width)"
+      ],
+      "metadata": {
+        "id": "iMlx8D30FxqO"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
